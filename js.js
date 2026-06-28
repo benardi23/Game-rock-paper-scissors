a2365a2de1e94289c2b3bb4efa4fde7e441629ee
benardi23 <benardduraku9@gmail.com>
2026-06-28 11:46:35 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,33 @@
+let page = document.getElementById("blue")
+page.addEventListener("click",()=>{
+document.body.style.backgroundColor = "blue";
+});
+let page1 = document.getElementById("yellow")
+page1.addEventListener("click",()=>{
+document.body.style.backgroundColor = "yellow";
+});
+let page2 = document.getElementById("pink")
+page2.addEventListener("click",()=>{
+document.body.style.backgroundColor = "pink";
+});
+let page3 = document.getElementById("grey")
+page3.addEventListener("click",()=>{
+document.body.style.backgroundColor = "grey";
+});
+let page4 = document.getElementById("green")
+page4.addEventListener("click",()=>{
+document.body.style.backgroundColor = "green";
+});
+let page5 = document.getElementById("red")
+page5.addEventListener("click",()=>{
+document.body.style.backgroundColor = "red";
+});
+let page6 = document.getElementById("purple")
+page6.addEventListener("click",()=>{
+document.body.style.backgroundColor = "purple";
+});
+let page7 = document.getElementById("black")
+page7.addEventListener("click",()=>{
+document.body.style.backgroundColor = "black";
+});
+
